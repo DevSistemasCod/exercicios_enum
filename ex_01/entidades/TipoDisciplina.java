ef62ae8ea401908908135016c86b7b7696ac4d82
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-05-15 10:07:09 -0300

    Add files via upload

---
+++ ex_01/entidades/TipoDisciplina.java
@@ -0,0 +1,19 @@
+package entidades;
+
+public enum TipoDisciplina {
+    FPOO("Fundamentos de Programação Orientada a Objetos"),
+    REDES("Redes de Computadores"),
+    SO("Sistemas Operacionais"),
+    LM("Linguagem de Marcação");
+    
+
+    private String descricao;
+
+	TipoDisciplina(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
