cd933d10a856ca5423d23a1e709af542e814b851
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-06-05 13:41:42 -0300

    Update TipoLivro.java

--- ex_02/entidades/TipoLivro.java
+++ ex_02/entidades/TipoLivro.java
@@ -1,12 +1,12 @@
 package entidades;
 
 public enum TipoLivro {
-	ROMANCE("romance"),
-	FICCAO_CIENTIFICA("ficção científica"),
-	AVENTURA("aventura"),
-	TUTORIAL("tútorial cientifico");
+    ROMANCE("romance"),
+    FICCAO_CIENTIFICA("ficção científica"),
+    AVENTURA("aventura"),
+    TUTORIAL("tútorial cientifico");
 	
-	private String descricao;
+    private String descricao;
 
     private TipoLivro(String descricao) {
         this.descricao = descricao;
